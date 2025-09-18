e58e557b4dd9b00addd6b09fe929d14e6f62690f
brian-mokaya <brianmokaya@gmail.com>
2025-09-18 23:29:13 +0300

    Add application startup log, adjust POM formatting, include H2 dependency, and update .gitignore

--- src/main/java/com/mokaya/tikio/TikioApplication.java
+++ src/main/java/com/mokaya/tikio/TikioApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TikioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TikioApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(TikioApplication.class, args);
+
+        System.out.println("Tikio Started Successfully");
+    }
 
 }
