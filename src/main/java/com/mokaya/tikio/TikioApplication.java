b74c56dedb6ca17bafa7c2091b60dbdcd283ef29
brian-mokaya <brianmokaya@gmail.com>
2025-09-18 23:28:22 +0300

    first commit

---
+++ src/main/java/com/mokaya/tikio/TikioApplication.java
@@ -0,0 +1,13 @@
+package com.mokaya.tikio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mokaya/tikio/TikioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mokaya.tikio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
